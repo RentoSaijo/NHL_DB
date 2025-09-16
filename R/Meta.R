81e6781e71d5eb1117d7b99b2b39a058b31d0926
RentoSaijo <rentosaijo0527@gmail.com>
2025-09-16 14:34:29 -0400

    Get all games

--- R/Meta.R
+++ R/Meta.R
@@ -4,5 +4,5 @@ suppressMessages(library(nhlscraper))
 
 # Get all games from 1917-1918 to 2024-2025.
 NHL_Games_19171918_20242025 <- get_games() %>% 
-  filter(season < 20242025)
+  filter(season <= 20242025)
 write_csv(NHL_Games_19171918_20242025, 'data/meta/NHL_Games_19171918_20242025.csv')
