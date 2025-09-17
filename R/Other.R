f4f71a7adaaf682c4ab7f79866ed565f62f4b839
RentoSaijo <rentosaijo0527@gmail.com>
2025-09-16 21:51:45 -0400

    Get countries

--- R/Other.R
+++ R/Other.R
@@ -5,4 +5,31 @@ suppressMessages(library(nhlscraper))
 # Get terminologies registered by 09-16-2025.
 NHL_Glossary_09_16_2025 <- get_glossary() %>% 
   arrange(id)
-write_csv(NHL_Glossary_09_16_2025, 'data/other/NHL_Glossary_09_16_2025.csv')
+write_csv(
+  NHL_Glossary_09_16_2025, 
+  'data/other/NHL_Glossary_09_16_2025.csv'
+)
+
+# Get countries registered by 09-16-2025.
+NHL_Countries_09_16_2025 <- get_countries() %>% 
+  arrange(id)
+write_csv(
+  NHL_Countries_09_16_2025, 
+  'data/other/NHL_Countries_09_16_2025.csv'
+)
+
+# Get terminologies registered by 09-16-2025.
+NHL_Glossary_09_16_2025 <- get_glossary() %>% 
+  arrange(id)
+write_csv(
+  NHL_Glossary_09_16_2025, 
+  'data/other/NHL_Glossary_09_16_2025.csv'
+)
+
+# Get terminologies registered by 09-16-2025.
+NHL_Glossary_09_16_2025 <- get_glossary() %>% 
+  arrange(id)
+write_csv(
+  NHL_Glossary_09_16_2025, 
+  'data/other/NHL_Glossary_09_16_2025.csv'
+)
