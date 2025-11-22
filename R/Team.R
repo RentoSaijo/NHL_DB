fc631dbb785f20ea18e8e2b079093fd801462d70
RentoSaijo <rentosaijo0527@gmail.com>
2025-11-22 10:28:37 -0500

    Update teams

--- R/Team.R
+++ R/Team.R
@@ -2,13 +2,13 @@
 suppressMessages(library(tidyverse))
 suppressMessages(library(nhlscraper))
 
-# Get all teams from 1917-1918 to 2024-2025.
-NHL_Teams_19171918_20242025 <- get_teams() %>% 
-  filter(!id %in% c(70, 99, 68)) %>% 
+# Get all teams from 1917-1918 to 2025-2026.
+NHL_Teams_19171918_20252026 <- get_teams() %>% 
+  filter(!id %in% c(70, 99)) %>% 
   arrange(id)
 write_csv(
-  NHL_Teams_19171918_20242025, 
-  'data/team/meta/NHL_Teams_19171918_20242025.csv'
+  NHL_Teams_19171918_20252026, 
+  'data/team/meta/NHL_Teams_19171918_20252026.csv'
 )
 
 # Get all franchises from 1917-1918 to 2024-2025.
